ec6f10b430402673786a20449765913ef723ecb1
Sreekanth Garika <sreekanth@sujainfo.com>
2025-07-29 17:28:30 +0530

    Initial Commit

---
+++ ProfJohnGallaugher_SwiftUI_Projects/ProfJohnGallaugher_SwiftUI_ProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfJohnGallaugher_SwiftUI_ProjectsApp.swift
+//  ProfJohnGallaugher_SwiftUI_Projects
+//
+//  Created by Suja Associates on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfJohnGallaugher_SwiftUI_ProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
