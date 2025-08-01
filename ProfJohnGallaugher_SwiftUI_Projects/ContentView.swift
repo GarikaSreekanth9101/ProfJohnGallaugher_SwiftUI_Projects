84040a444b94db4ff960691abd6ec2e56bd17de4
Sreekanth Garika <sreekanth@sujainfo.com>
2025-08-01 17:13:24 +0530

    Ch. 1.8 An Into to Types in Swift - SwiftUI course (2025)

--- ProfJohnGallaugher_SwiftUI_Projects/ContentView.swift
+++ ProfJohnGallaugher_SwiftUI_Projects/ContentView.swift
@@ -9,21 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
+    @State private var imageString: String = ""
+    @State private var message: String = ""
+    
+    @State private var count: Int = 0
+    
+//    private var imgArray: [String] = ["image0","image1","image2","image3","image4","image5","image6","image7","image8","image9"]
+    
+    
     var body: some View {
         
         VStack {
-            Image(systemName: "swift")
+            Spacer()
+            Image(imageString)
                 .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+                .scaledToFill()
+                .frame(width: 300, height: 300)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+            Text(message)
                 .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.orange)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Spacer()
+            Button("Show Message"){
+            
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great!"
+                
+                self.message = self.message == message1 ? message2 : message1
+                self.imageString = "image\(count)"
+                
+                count += 1
+                
+                if count > 9{
+                    count = 0
+                }
+                
+              
+                
+//                if count > 9{
+//                    count = 0
+//                    self.message = self.message == message1 ? message2 : message1
+////                    self.imageString = imgArray[count]
+//                    self.imageString = "image\(count)"
+//                }else{
+//                    self.message = self.message == message1 ? message2 : message1
+////                    self.imageString = imgArray[count]
+//                    self.imageString = "image\(count)"
+//                }
+               
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
+        
     }
-    
 }
 
 #Preview {
