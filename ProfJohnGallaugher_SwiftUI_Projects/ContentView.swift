3a0c5bf460b07912f733169a06635a1ae77866e7
Sreekanth Garika <sreekanth@sujainfo.com>
2025-08-04 12:48:57 +0530

    Ch. 1.9 An Intro to Arrays in Swift & SwiftUI (2025)

--- ProfJohnGallaugher_SwiftUI_Projects/ContentView.swift
+++ ProfJohnGallaugher_SwiftUI_Projects/ContentView.swift
@@ -15,7 +15,9 @@ struct ContentView: View {
     
     @State private var count: Int = 0
     
-//    private var imgArray: [String] = ["image0","image1","image2","image3","image4","image5","image6","image7","image8","image9"]
+    private var imgArray: [String] = ["image0","image1","image2","image3","image4","image5","image6","image7","image8","image9"]
+    
+    private var messageArray: [String] = ["When I'm around you I feel like I could fly","You're Magic","I sleep 18 hrs a day, but I'm glad I got up to see you!","Anybody seen Nemo?","Wow! I'm glad I came out. I didn't expect to see you!","Whoos' that?\nAwesomeness in human form","Plenty of fish in the sea but none like you!","You bring me out of my shell!","Prepare for Bease Mode","It's Good to Have Friends"]
     
     
     var body: some View {
@@ -24,22 +26,25 @@ struct ContentView: View {
             Spacer()
             Image(imageString)
                 .resizable()
-                .scaledToFill()
-                .frame(width: 300, height: 300)
+                .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             Spacer()
             Button("Show Message"){
             
                 let message1 = "You Are Awesome!"
                 let message2 = "You Are Great!"
                 
-                self.message = self.message == message1 ? message2 : message1
-                self.imageString = "image\(count)"
+//                self.message = self.message == message1 ? message2 : message1
+//                self.imageString = "image\(count)"
+                
+                self.message = messageArray[count]
+                self.imageString = imgArray[count]
                 
                 count += 1
                 
