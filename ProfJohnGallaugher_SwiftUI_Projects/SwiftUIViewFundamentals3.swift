090bf5992b7db533fc8c4b0e780bd0f752f283fd
Sreekanth Garika <sreekanth@sujainfo.com>
2025-08-07 14:05:47 +0530

    Ch. 1. SwiftUI Fundamentals: Getting Layout Right Pt 2. Colors, background, safeArea & More (2025)

---
+++ ProfJohnGallaugher_SwiftUI_Projects/SwiftUIViewFundamentals3.swift
@@ -0,0 +1,79 @@
+//
+//  SwiftUIViewFundamentals3.swift
+//  ProfJohnGallaugher_SwiftUI_Projects
+//
+//  Created by Suja Associates on 07/08/25.
+//
+
+import SwiftUI
+
+struct SwiftUIViewFundamentals3: View {
+    
+    @State private var message: String = "You Are Awesome!"
+    
+    var body: some View {
+        
+        //To set background color we can keep in ZStack or
+        //Bellow the VStack also we can change
+        
+        //        ZStack {
+        //            //For background Color
+        //            Color(.gray)
+        //                .ignoresSafeArea()
+        //        }
+        
+        
+        VStack(){
+            
+            
+            Text("You have skills!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+                .foregroundStyle(.skyBlue)
+                .multilineTextAlignment(.center)
+            
+                .padding()
+                .background(.accent)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+            Spacer()
+            Text(message)
+                .foregroundStyle(.accent)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            HStack{
+                Button("Awesome") {
+                    message = "You Are Awesome!"
+                }
+                //                .tint(.fernandez)
+                Spacer()
+                Button("Great") {
+                    message = "You Are Great!"
+                }
+                //                .tint(.gray)
+            }
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(.white)
+            .font(.title2)
+            .tint(.launchScreenBG)
+            .padding()
+            
+        }
+        //            .background(.black.gradient)
+        //Gradient Colors
+//        .background(
+//            Gradient(colors: [.fernandez, .skyBlue, .launchScreenBG, .accent])
+//        )
+    }
+}
+
+#Preview {
+    SwiftUIViewFundamentals3()
+}
