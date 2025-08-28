3f8e4c9463c5ab92bcd3ef39c8de033c2d8d8ec5
Sreekanth Garika <sreekanth@sujainfo.com>
2025-08-28 16:10:30 +0530

    Ch. 3.2 using scaleEffect, spring, and withAnimation to animate Bip The Guy in SwiftUI (2025)

--- ProfJohnGallaugher_SwiftUI_Projects/ContentView.swift
+++ ProfJohnGallaugher_SwiftUI_Projects/ContentView.swift
@@ -10,7 +10,54 @@ import AVFAudio
 
 struct ContentView: View {
     
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var isFullSize: Bool = true
+    
     var body: some View {
+        
+        VStack{
+            Spacer()
+            //MARK: Image
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(isFullSize ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    isFullSize = false // will imedeiately shrink using .scalEffect to 90% of size
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {
+                        isFullSize = true//will go from 90% to 100% size but using the .spring animation
+                    }
+                }
+            Spacer()
+            //MARK: Button
+            Button {
+                //TODO: Button Action Here
+                
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+        }
+        .padding()
+    }
+    
+    func playSound(soundName: String){
+        
+        if audioPlayer != nil && audioPlayer.isPlaying{
+            audioPlayer.stop()
+        }
+        
+        guard let soundFile = NSDataAsset(name: soundName) else{
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }catch{
+            print("😡 ERROR: \(error.localizedDescription) creating audio player")
+        }
         
     }
     
